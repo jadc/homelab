dea2d9da6061085efe8f48f4eeb76b880ed1da24
jadc <chehimj@gmail.com>
2025-09-14 21:19:29 -0600

    wireguard: removed old/redundant code

--- config/service/wireguard/default.nix
+++ config/service/wireguard/default.nix
@@ -31,19 +31,10 @@ in
             ips = [ "${prefixIPv4}.1/24" "${prefixIPv6}::1/64" ];
             privateKeyFile = "${inputs.secrets}/wireguard-private.key";
 
-            postSetup    = "${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${prefixIPv4}.0/24 -o ${externalInterface} -j MASQUERADE";
-            postShutdown = "${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${prefixIPv4}.0/24 -o ${externalInterface} -j MASQUERADE";
-
             peers = [
-                # Laptop
-                {
-                    publicKey = "H0Ux2fL64k6V9rG/BEegP0hNpye/gYNs6R1o/dIXNSg=";
-                    allowedIPs = [ "${prefixIPv4}.2/32" "${prefixIPv6}::2/128" ];
-                }
-                # iPhone
                 {
                     publicKey = "F5dy2UCbUGr9O3Qf5VMYrg3s49qlfNL4bmYPWUWWKQo=";
-                    allowedIPs = [ "${prefixIPv4}.3/32" "${prefixIPv6}::3/128" ];
+                    allowedIPs = [ "${prefixIPv4}.2/32" "${prefixIPv6}::2/128" ];
                 }
             ];
         };
