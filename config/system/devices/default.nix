d9218a383c882c655e54dc8240cdfa2f22d6092e
jad <chehimj@gmail.com>
2025-09-21 21:00:46 -0700

    options: added options for mounts (#2)

---
+++ config/system/devices/default.nix
@@ -0,0 +1,78 @@
+{ config, lib, ... }:
+
+let
+    name = "devices";
+in
+{
+    options.homelab.system.${name} = with lib; mkOption {
+        default = {};
+        type = with types; attrsOf (submodule {
+            options = {
+                enable = mkEnableOption "this device mount" // {
+                    default = true;
+                };
+
+                device = mkOption {
+                    type = types.str;
+                    description = "Device to be mounted";
+                    example = "/dev/sda1";
+                };
+
+                fsType = mkOption {
+                    type = types.str;
+                    description = "Type of file system";
+                    default = "auto";
+                };
+
+                options = mkOption {
+                    type = types.listOf types.str;
+                    description = "Mount options";
+                    default = [ "defaults" ];
+                };
+
+                group = mkOption {
+                    type = types.nullOr types.str;
+                    description = "Group that owns the mount point";
+                    default = null;
+                };
+
+                permissions = mkOption {
+                    type = types.str;
+                    description = "Permissions on the mount point";
+                    default = "755";
+                };
+
+                owner = mkOption {
+                    type = types.str;
+                    description = "User that owns the mount point";
+                    default = "root";
+                };
+            };
+        });
+    };
+
+    config = let
+        cfg = config.homelab.system.${name};
+        enabled = lib.filterAttrs (mountPoint: x: x.enable) cfg;
+    in {
+        # Create mountPoint directory
+        systemd.tmpfiles.rules = with lib;
+            mapAttrsToList (mountPoint: x:
+                let
+                    group = if x.group != null then x.group else "-";
+                in
+                    "d ${mountPoint} ${x.permissions} ${x.owner} ${group} - -"
+            ) enabled;
+
+        # Mount device to mountPoint
+        fileSystems = with lib; mkMerge (
+            mapAttrsToList (mountPoint: x: {
+                ${mountPoint} = {
+                    device = x.device;
+                    fsType = x.fsType;
+                    options = x.options;
+                };
+            }) enabled
+        );
+    };
+}
--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 {
     imports = [
         # System
+        ./config/system/devices
         ./config/system/locale
         ./config/system/misc
         ./config/system/tlp
@@ -18,6 +19,30 @@
         system = {
             timeZone = "America/Edmonton";
             locale = "en_CA.UTF-8";
+
+            devices = {
+                "/data/media" = {
+                    device = "/dev/disk/by-uuid/d91b87c6-61f9-4901-99ee-efde2b36bbeb";
+                    fsType = "xfs";
+                    group = "media";
+                };
+            };
+        };
+
+        service = {
+            wireguard = let cfg = config.homelab.service.wireguard; in {
+                interface = "eno1";
+                ipv4Prefix = "10.66.66";
+                ipv6Prefix = "fd42:42:42";
+                peers = [
+                    {
+                        publicKey = "F5dy2UCbUGr9O3Qf5VMYrg3s49qlfNL4bmYPWUWWKQo=";
+                        allowedIPs = [ "${cfg.ipv4Prefix}.2/32" "${cfg.ipv6Prefix}::2/128" ];
+                    }
+                ];
+                port = 443;
+                privateKeyFile = "${inputs.secrets}/wireguard-private.key";
+            };
         };
 
         service = {
