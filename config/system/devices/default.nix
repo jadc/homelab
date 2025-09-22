c3a96249f94feda38bacabf588ce074b7e0b596a
jadc <chehimj@gmail.com>
2025-09-21 20:48:08 -0700

    wip

--- config/system/devices/default.nix
+++ config/system/devices/default.nix
@@ -5,75 +5,69 @@ let
 in
 {
     options.homelab.system.${name} = with lib; mkOption {
-        type = with types; listOf (submodule {
-            enable = mkEnableOption {
-                default = true;
-            };
+        type = with types; attrsOf (submodule {
+            options = {
+                enable = mkEnableOption "this device mount" // {
+                    default = true;
+                };
 
-            device = mkOption {
-                type = types.str;
-                description = "Device to be mounted";
-                example = "/dev/sda1";
-            };
+                device = mkOption {
+                    type = types.str;
+                    description = "Device to be mounted";
+                    example = "/dev/sda1";
+                };
 
-            mountPoint = mkOption {
-                type = types.str;
-                description = "Path to mount the device";
-                example = "/mnt/data";
-            };
+                fsType = mkOption {
+                    type = types.str;
+                    description = "Type of file system";
+                    default = "auto";
+                };
 
-            fsType = mkOption {
-                type = types.str;
-                description = "Type of file system";
-                default = "auto";
-            };
+                options = mkOption {
+                    type = types.listOf types.str;
+                    description = "Mount options";
+                    default = [ "defaults" ];
+                };
 
-            options = mkOption {
-                type = types.listOf types.str;
-                description = "Mount options";
-                default = [ "defaults" ];
-            };
+                group = mkOption {
+                    type = types.nullOr types.str;
+                    description = "Group that owns the mount point";
+                    default = null;
+                };
 
-            group = mkOption {
-                type = types.nullOr types.str;
-                description = "Group that owns the mount point";
-                default = null;
-            };
+                permissions = mkOption {
+                    type = types.str;
+                    description = "Permissions on the mount point";
+                    default = "755";
+                };
 
-            permissions = mkOption {
-                type = types.str;
-                description = "Permissions on the mount point";
-                default = "755";
-            };
-
-            owner = mkOption {
-                type = types.str;
-                description = "User that owns the mount point";
-                default = "root";
+                owner = mkOption {
+                    type = types.str;
+                    description = "User that owns the mount point";
+                    default = "root";
+                };
             };
         });
     };
 
     config = let
         cfg = config.homelab.system.${name};
-        enabled = lib.filterAttrs (name: x: x.enable) cfg;
+        enabled = lib.filterAttrs (mountPoint: x: x.enable) cfg;
     in {
         # Create mountPoint directory
         systemd.tmpfiles.rules = with lib; mkMerge (
-            mapAttrsToList (name: x:
-                optional x.enable (
-                    let
-                        groupPart = if x.group != null then x.group else "-";
-                    in
-                        "d ${x.mountPoint} ${x.permissions} ${x.owner} ${groupPart} - -"
-                )
+            mapAttrsToList (mountPoint: x:
+                let
+                    group = if x.group != null then x.group else "-";
+                in
+                    "d ${mountPoint} ${x.permissions} ${x.owner} ${group} - -"
             ) enabled
         );
 
         # Mount device to mountPoint
         fileSystems = with lib; mkMerge (
-            mapAttrsToList (name: x: {
-                ${x.mountPoint} = {
+            mapAttrsToList (mountPoint: x: {
+                ${mountPoint} = {
                     device = x.device;
                     fsType = x.fsType;
                     options = x.options;
--- configuration.nix
+++ configuration.nix
@@ -19,28 +19,24 @@
             timeZone = "America/Edmonton";
             locale = "en_CA.UTF-8";
 
-            devices = [
-                {
+            devices = {
+                "/data/media" = {
                     device = "/dev/disk/by-uuid/d91b87c6-61f9-4901-99ee-efde2b36bbeb";
-                    mountPoint = "/data/media";
                     fsType = "xfs";
                     group = "media";
-                }
-            ];
+                };
+            };
         };
 
         service = {
-            wireguard = {
+            wireguard = let cfg = config.homelab.service.wireguard; in {
                 interface = "eno1";
                 ipv4Prefix = "10.66.66";
                 ipv6Prefix = "fd42:42:42";
-                peers = let
-                    ipv4 = config.homelab.service.wireguard.ipv4Prefix;
-                    ipv6 = config.homelab.service.wireguard.ipv6Prefix;
-                in [
+                peers = [
                     {
                         publicKey = "F5dy2UCbUGr9O3Qf5VMYrg3s49qlfNL4bmYPWUWWKQo=";
-                        allowedIPs = [ "${ipv4}.2/32" "${ipv6}::2/128" ];
+                        allowedIPs = [ "${cfg.ipv4Prefix}.2/32" "${cfg.ipv6Prefix}::2/128" ];
                     }
                 ];
                 port = 443;
