120f8dae50f10193788d7ff183d1d4e9908345f8
jadc <chehimj@gmail.com>
2026-01-04 19:31:22 -0700

    wip

--- config/system/kernel/default.nix
+++ config/system/kernel/default.nix
@@ -100,6 +100,18 @@ in
                 ];
         };
 
+        # Remove NVIDIA PCI devices from the system when disabled
+        services.udev.extraRules = mkIf (!self.flags.nvidia) ''
+            # Remove NVIDIA USB xHCI Host Controller devices, if present
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+            # Remove NVIDIA USB Type-C UCSI devices, if present
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+            # Remove NVIDIA Audio devices, if present
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+            # Remove NVIDIA VGA/3D controller devices
+            ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+        '';
+
         services.xserver.videoDrivers = mkIf self.flags.intel [ "modesetting" ];
 
         hardware.graphics = mkIf self.flags.intel {
