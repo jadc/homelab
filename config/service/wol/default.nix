56fb27bdbb336aeab0f38488eafe0c1721308e9a
jadc <chehimj@gmail.com>
2026-01-17 20:01:00 -0700

    .

--- config/service/wol/default.nix
+++ config/service/wol/default.nix
@@ -30,7 +30,7 @@ let
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('''''', PORT))
+        sock.bind(("", PORT))
         print(f"Listening for WOL packets on port {PORT}...")
         sys.stdout.flush()
 
